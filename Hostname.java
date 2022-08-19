1b2f5f1ee1cfb4f78038ba4e4c386f0490b4d16c
Simon Reber <sreber84@users.noreply.github.com>
2022-08-19 13:03:30 +0200

    Update Hostname.java

--- Hostname.java
+++ Hostname.java
@@ -3,7 +3,7 @@ import java.net.UnknownHostException;
 
 public class Hostname {
    public static void main(String[]args) throws UnknownHostException{
-        String dnsHostname = System.getenv("DNSHOSTNAME");
+    String dnsHostname = System.getenv("DNSHOSTNAME");
         InetAddress address = InetAddress.getByName(dnsHostname);
 
         System.out.println("my local machine: " + address);
